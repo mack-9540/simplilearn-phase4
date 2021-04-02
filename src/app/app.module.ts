29677f00b9075d3971d3b6140508835735d78a4c
Mayank Singh <mayanksingh.nbd@gmail.com>
2021-04-02 22:59:50 +0530

    simplilearn phase 4 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+import { MainComponent } from './main/main.component';
+import { QuestionComponent } from './question/question.component';
+import { ReviewComponent } from './review/review.component';
+import { DisplayComponent } from './display/display.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
+import { applicationRoutes } from './app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule} from "@angular/forms";
+import { QuestionService } from './model/question.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    MainComponent,
+    
+    QuestionComponent,
+    
+    ReviewComponent,
+    
+    DisplayComponent,
+    
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(applicationRoutes),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [QuestionService],
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
